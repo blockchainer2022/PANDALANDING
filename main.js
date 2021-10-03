cb55b85a4db6326ea21381c2dcdf84c756234890
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-10-03 12:12:38 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const countOnContainer = document.querySelector(".count_on_container");
+const countOffContainer = document.querySelector(".count_off_container");
+
+// get the timestamp of desired date from https://www.epochconverter.com/ and paste it as first parameter of Flipdown()
+new FlipDown(1633602600, {
+  headings: ["DAYS", "HRS", "MIN", "SEC"],
+  theme: "dark",
+})
+  .start()
+  .ifEnded(() => {
+    countOnContainer.classList.add("hidden_ut");
+    countOffContainer.classList.add("display_ut");
+  });
