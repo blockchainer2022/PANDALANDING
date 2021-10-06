c5bf0c1fcfba4a54cd865c36bb35e2378a56232b
blockchainer2022 <=blockchainer.2202@gmail.com>
2021-10-06 19:35:55 +0530

    update time

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const countOnContainer = document.querySelector(".count_on_container");
 const countOffContainer = document.querySelector(".count_off_container");
 
 // get the timestamp of desired date from https://www.epochconverter.com/ and paste it as first parameter of Flipdown()
-new FlipDown(1633602600, {
+new FlipDown(1634394600, {
   headings: ["DAYS", "HRS", "MIN", "SEC"],
   theme: "dark",
 })
